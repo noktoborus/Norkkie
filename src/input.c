b03de9b2d8be45e7af6dac6f9d22661ddb4f65b0
Noktoborus <noktoborus@gmail.com>
2011-01-16 22:27:28 +1000

    input: filter coming soon

--- src/input.c
+++ src/input.c
@@ -1,3 +1,25 @@
 #include "input.h"
 
+int
+input_filter (char type, unsigned char in)
+{
+	switch (type)
+	{
+		case FINPUT_TSTRING:
+			/* true to all symbols ? */
+			return 0;
+		case FINPUT_TFLOAT:
+			if (in == '.')
+				return true;
+		case FINPUT_TSINT:
+			if (in == '-')
+				return true;
+		case FINPUT_TUINT:
+			if (in == '+' || (in >= '0' && in <= '9'))
+				return true;
+		case FINPUT_TVOID:
+			/* allways false ;3 */
+	};
+	return 1;
+}
 
